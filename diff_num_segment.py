6303689bfab1af76b0c87bb71a25d2e59d3001f9
Nikita Lyakhovoy <lyakhovoynikita@gmail.com>
2023-06-30 11:50:18 +0100

    Test image recognition using pytesseract

--- diff_num_segment.py
+++ diff_num_segment.py
@@ -1,15 +1,9 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-# matplotlib inline
-import imutils
-import zipfile
+import os
 
 #Load image and convert to grey.
-img = cv2.imread('captcha_imgs/captcha_0.jpg', 0)
-
-cv2.imshow("1", np.array(img))
-cv2.waitKey(0)
+img = cv2.imread('captcha_imgs/captcha_65.jpg', 0)
 
 # From RGB to BW
 # Adaptive thresholding
@@ -22,10 +16,30 @@ ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 blur = cv2.GaussianBlur(img, (5, 5), 0)
 ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-#Plot
-titles = ['Original', 'Adaptive', 'Otsu', 'Gaussian + Otsu']
-images = [img, th, th2, th3]
-for i in range(4):
-    plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
-    plt.title(titles[i])
-    plt.xticks([]), plt.yticks([])
+kernel = np.ones((3,3), np.uint8)
+
+dilation = cv2.dilate(th3, kernel, iterations=1)
+
+erosion = cv2.erode(dilation, kernel, iterations=1)
+
+kernel = np.ones((3,1), np.uint8)
+dilation = cv2.dilate(erosion, kernel, iterations=1)
+
+# Prepare output directory
+os.makedirs("output", exist_ok=True)
+
+#Get the individual letters.
+x, y, w, h = 22, 10, 20, 38
+for  i in range(6):
+    # # get the bounding rect
+    # cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # cv2.rectangle(dilation, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # save each character to a separate image
+    digit = dilation[y:y+h, x:x+w]
+
+    # Add 5-pixel white padding
+    digit = cv2.copyMakeBorder(digit, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+
+    cv2.imwrite(f'output/char_{i}.png', digit)
+    x += w
